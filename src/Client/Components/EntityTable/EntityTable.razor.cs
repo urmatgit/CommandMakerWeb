963682f2ab8f1fb7bde348e4dce89637eaba87e7
urmatgit <urmat2g@gmail.com>
2024-09-12 17:00:51 +0300

    fix bug checkbox

--- src/Client/Components/EntityTable/EntityTable.razor.cs
+++ src/Client/Components/EntityTable/EntityTable.razor.cs
@@ -78,7 +78,7 @@ public partial class EntityTable<TEntity, TId, TRequest>
             ((bool.TryParse(action, out bool isTrue) && isTrue) || // check if action equals "True", then it's allowed
             (Context.EntityResource is { } resource && await AuthService.HasPermissionAsync(state.User, action, resource)));
 
-    private bool HasActions => _canUpdate || _canDelete || (Context.HasExtraActionsFunc is not null && Context.HasExtraActionsFunc());
+    public bool HasActions => _canUpdate || _canDelete || (Context.HasExtraActionsFunc is not null && Context.HasExtraActionsFunc());
     private bool CanUpdateEntity(TEntity entity) => _canUpdate && (Context.CanUpdateEntityFunc is null || Context.CanUpdateEntityFunc(entity));
     private bool CanDeleteEntity(TEntity entity) => _canDelete && (Context.CanDeleteEntityFunc is null || Context.CanDeleteEntityFunc(entity));
 
--- src/Client/Pages/Catalog/Games.razor.cs
+++ src/Client/Pages/Catalog/Games.razor.cs
@@ -1,17 +1,39 @@
 ﻿
 using FSH.BlazorWebAssembly.Client.Components.EntityTable;
 using FSH.BlazorWebAssembly.Client.Infrastructure.ApiClient;
+using FSH.BlazorWebAssembly.Client.Infrastructure.Auth;
 using FSH.BlazorWebAssembly.Client.Shared;
 using FSH.WebApi.Shared.Authorization;
 using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor;
 using static MudBlazor.CategoryTypes;
 
 namespace FSH.BlazorWebAssembly.Client.Pages.Catalog;
 public partial class Games
 {
+    [CascadingParameter]
+    protected Task<AuthenticationState> AuthState { get; set; } = default!;
+    [Inject]
+    protected IAuthorizationService AuthService { get; set; } = default!;
+
+    private bool _canViewRoles;
+    private bool _canViewUsers;
+    private bool _canViewTenants;
+    private bool CanViewAdministrationGroup => _canViewUsers || _canViewRoles || _canViewTenants;
+    protected override async Task OnParametersSetAsync()
+    {
+        var user = (await AuthState).User;
+        _canViewRoles = await AuthService.HasPermissionAsync(user, FSHAction.View, FSHResource.Roles);
+        _canViewUsers = await AuthService.HasPermissionAsync(user, FSHAction.View, FSHResource.Users);
+
+        _canViewTenants = await AuthService.HasPermissionAsync(user, FSHAction.View, FSHResource.Tenants);
+    }
     protected EntityServerTableContext<GameDto, Guid, UpdateGameRequest> Context { get; set; } = default!;
     protected override void OnInitialized() =>
+
         Context = new(
             entityName: L["Game"],
             entityNamePlural: L["Games"],
@@ -31,6 +53,7 @@ public partial class Games
             createFunc: async brand => await GamesClient.CreateAsync(brand.Adapt<CreateGameRequest>()),
             updateFunc: async (id, brand) => await GamesClient.UpdateAsync(id, brand),
             deleteFunc: async id => await GamesClient.DeleteAsync(id),
+            hasExtraActionsFunc: () => !CanViewAdministrationGroup,
             exportAction: string.Empty);
     public void ShowGameDialog(ISnackbar snack)
     {
